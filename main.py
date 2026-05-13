1923a2c0fb0d4f552da5ed4d9b450a8e2dd751de
Cristopher <cristophercruzcorrea7@gmail.com>
2026-05-12 21:44:05 -0500

    feat: implementar bucle for para notas

--- main.py
+++ main.py
@@ -11,32 +11,3 @@ while opc1 == "si": #Añadimos un ciclo para que el programa se repita hasta que
     for i in range(5): #Añadimos un ciclo para que el usuario ingrese las notas del estudiante
 
         not1=input("Ingrese una nota: ")
-
-        for let1 in not1:
-
-            while let1 not in "0123456789.": #Añadimos validacion para que no se ingresen letras
-                print("No se permiten letras")
-                not1=input("Ingrese nuevamente la nota: ")
-                let1=not1
-
-        not1=float(not1) #Convertimos la nota a un numero decimal para poder realizar operaciones matematicas con ella
-
-        while not1 < 1.0 or not1 > 5.0: #Añadimos validacion para que no se ingresen notas menores a 1.0 o mayores a 5.0
-            print("Nota no válida")
-            not1=float(input("Ingrese nuevamente la nota: "))
-
-        sum1=sum1+not1 #Sumamos las notas para saber el total de las notas del estudiante
-
-    prom1=sum1/5 #Calculamos el promedio del estudiante
-
-    print("Estudiante:", nom1)
-    print("Promedio final:", prom1)
-
-    if prom1>=3.5: #Añadimos una condicion para saber si el estudiante aprobo o reprobo, en este caso se considera que un estudiante aprueba si su promedio es mayor o igual a 3.5
-        print("Aprobó")
-
-    else:
-        print("Reprobado")
-
-    opc1=input("¿Quiere ingresar las notas de otro estudiante? Ingrese 'si' o 'no': ") #Al final del ciclo preguntamos al usuario si desea ingresar las notas de otro estudiante, si el usuario ingresa "si" el ciclo se repetira, de lo contrario el programa terminara
-
