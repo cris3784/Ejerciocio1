b3b806fc0548a625a05a9d04d1335e65a3ad214b
Cristopher <cristophercruzcorrea7@gmail.com>
2026-05-12 21:34:17 -0500

    feat: Agregar variables y captura de datos

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Bienvenido, en este programa podra saber si aprobo o reprobo un estudiante")
+
+opc1="si" #Añadimos una variable para controlar el ciclo del programa, esta variable se inicializa con "si" para que el programa se ejecute al menos una vez
+
+while opc1 == "si": #Añadimos un ciclo para que el programa se repita hasta que el usuario decida no ingresar mas estudiantes
+
+    nom1=input("Ingrese el nombre del estudiante: ")
+
+    sum1=0 #Añadimos una variable para sumar las notas del estudiante y asi poder calcular el promedio al final del ciclo
+
